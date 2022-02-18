c9238f06a562206843ec8bf8d204be435c7081b8
Ziyu Huang <aprilhuang825@gmail.com>
2022-02-18 19:53:28 +0000

    refactored files

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const flatten = require('./flatten');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
-const letterPositions = require('./letterPositions');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 module.exports = {
   head,
   tail,
   middle,
   assertArraysEqual,
   assertEqual,
-  eqArrays,
-  flatten,
+  assertObjectsEqual,
+  countLetters,
   countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
   letterPositions,
-  findKeyByValue
+  map,
+  takeUntil,
+  without
 };
